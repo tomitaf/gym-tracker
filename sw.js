4480b3e5e5bd78c38ad6c284d5aab10557c48571
Felipe Monteiro Tomita <felipe.tomita@gmail.com>
2026-05-29 11:39:49 -0300

    v9: botoes lixeira trocados para btn_lixeira_fnd_preto.jpg

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const ASSETS = [
   './gym_tracker_v3.html',
   './manifest.json',
   './icone-app.jpg',
-  './btn_lixeira_trnsp.png',
+  './btn_lixeira_fnd_preto.jpg',
   'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.19.0/tabler-icons.min.css'
 ];
 
